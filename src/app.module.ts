6104b645d1035406ce51e7d3f407870701261a25
Degreet <degreetpro@gmail.com>
2026-01-17 21:00:55 +0200

    Add database configuration with TypeORM and environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,40 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import Joi from '@hapi/joi';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV ?? 'development'}`,
+      validationSchema: Joi.object({
+        DB_HOST: Joi.required(),
+        DB_PORT: Joi.number().default(5432),
+        DB_USER: Joi.required(),
+        DB_PASSWORD: Joi.required(),
+        DB_NAME: Joi.required(),
+      }),
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          type: 'postgres',
+          host: configService.get('DB_HOST'),
+          port: configService.get<number>('DB_PORT'),
+          username: configService.get('DB_USER'),
+          password: configService.get('DB_PASSWORD'),
+          database: configService.get('DB_NAME'),
+          autoLoadEntities: true,
+          synchronize: process.env.NODE_ENV !== 'production',
+          logging: process.env.NODE_ENV === 'development',
+        };
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
