fec0e127f307adc50b043fee09a5ab631d63b1ad
Degreet <degreetpro@gmail.com>
2026-01-21 12:53:04 +0200

    feat(auth): implement RefreshTokenIdsStorage

---
+++ src/iam/authentication/refresh-token-ids.storage.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { RedisService } from '../../common/redis/redis.service';
+
+@Injectable()
+export class RefreshTokenIdsStorage {
+  constructor(private readonly redisService: RedisService) {}
+
+  async insert(userId: number, tokenId: string) {
+    const redisClient = this.redisService.getClient();
+    await redisClient.set(this.getKey(userId), tokenId);
+  }
+
+  async validate(userId: number, tokenId: string) {
+    const redisClient = this.redisService.getClient();
+    const storedId = await redisClient.get(this.getKey(userId));
+    return storedId === tokenId;
+  }
+
+  private getKey(userId: number) {
+    return `user-${userId}`;
+  }
+}
