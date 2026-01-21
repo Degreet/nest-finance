91067487ea05437623e772f3326b24749944c445
Degreet <degreetpro@gmail.com>
2026-01-21 11:17:04 +0200

    feat(redis): implement RedisService with lifecycle hooks

---
+++ src/common/redis/redis.service.ts
@@ -0,0 +1,28 @@
+import {
+  Inject,
+  Injectable,
+  Logger,
+  OnApplicationShutdown,
+} from '@nestjs/common';
+
+import { REDIS_CLIENT } from './redis.constants';
+import Redis from 'ioredis';
+
+@Injectable()
+export class RedisService implements OnApplicationShutdown {
+  private readonly logger = new Logger(RedisService.name);
+
+  constructor(@Inject(REDIS_CLIENT) private readonly redisClient: Redis) {
+    this.redisClient.on('error', (err) => {
+      this.logger.error(`Redis error: ${err.message}`);
+    });
+  }
+
+  onApplicationShutdown() {
+    this.redisClient.quit();
+  }
+
+  getClient() {
+    return this.redisClient;
+  }
+}
