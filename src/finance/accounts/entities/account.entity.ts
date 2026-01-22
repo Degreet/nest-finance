72d3ca0e1881dfd695d72aabec09023276a777d4
Degreet <degreetpro@gmail.com>
2026-01-22 09:36:06 +0200

    feat(accounts): describe account entity

--- src/finance/accounts/entities/account.entity.ts
+++ src/finance/accounts/entities/account.entity.ts
@@ -1,4 +1,36 @@
-import { Entity } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { User } from '../../../users/entities/user.entity';
+import { AccountType } from '../enums/account-type.enum';
 
 @Entity()
-export class Account {}
+export class Account {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  user: User;
+
+  @Column()
+  name: string;
+
+  @Column({ type: 'enum', enum: AccountType })
+  type: AccountType;
+
+  @Column()
+  currency: string;
+
+  @Column({ type: 'decimal', precision: 15, scale: 2, default: 0 })
+  balance: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
