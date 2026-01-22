62626dbee3b89565af7fb99ebc942c5423524379
Degreet <degreetpro@gmail.com>
2026-01-22 14:47:08 +0200

    feat(accounts): describe CreateAccountDto

--- src/finance/accounts/dto/create-account.dto.ts
+++ src/finance/accounts/dto/create-account.dto.ts
@@ -1,1 +1,17 @@
-export class CreateAccountDto {}
+import { IsEnum, IsNumber, IsString, Length } from 'class-validator';
+import { AccountType } from '../enums/account-type.enum';
+
+export class CreateAccountDto {
+  @IsString()
+  name: string;
+
+  @IsEnum(AccountType)
+  type: AccountType;
+
+  @IsString()
+  @Length(3, 3)
+  currency: string;
+
+  @IsNumber()
+  initial_balance: number;
+}
