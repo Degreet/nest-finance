17cf898b5313c2e0cb500fa8c759f81f68cc6e72
Degreet <degreetpro@gmail.com>
2026-01-23 18:35:21 +0200

    feat(accounts): pass userId to create account method

--- src/finance/accounts/accounts.controller.ts
+++ src/finance/accounts/accounts.controller.ts
@@ -1,15 +1,28 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { AccountsService } from './accounts.service';
 import { CreateAccountDto } from './dto/create-account.dto';
 import { UpdateAccountDto } from './dto/update-account.dto';
+import { ActiveUser } from '../../iam/decorators/active-user.decorator';
+import type { ActiveUserData } from '../../iam/interfaces/active-user-data.interface';
 
 @Controller('accounts')
 export class AccountsController {
   constructor(private readonly accountsService: AccountsService) {}
 
   @Post()
-  create(@Body() createAccountDto: CreateAccountDto) {
-    return this.accountsService.create(createAccountDto);
+  create(
+    @Body() createAccountDto: CreateAccountDto,
+    @ActiveUser() user: ActiveUserData,
+  ) {
+    return this.accountsService.create(createAccountDto, user.sub);
   }
 
   @Get()
