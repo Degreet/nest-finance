ec8312baf88bc41e5827507145649689b04c8bee
Degreet <degreetpro@gmail.com>
2026-01-24 11:03:42 +0200

    feat(transactions): implement Transaction entity

--- src/finance/transactions/entities/transaction.entity.ts
+++ src/finance/transactions/entities/transaction.entity.ts
@@ -1,1 +1,39 @@
-export class Transaction {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { Account } from '../../accounts/entities/account.entity';
+
+@Entity()
+export class Transaction {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  type: 'income' | 'expense';
+
+  @ManyToOne(() => Account, (account) => account.transactions)
+  account: Account;
+
+  @Column({ type: 'decimal', precision: 15, scale: 2 })
+  amount: number;
+
+  @Column()
+  category: string;
+
+  @Column({ nullable: true })
+  description: string;
+
+  @Column({ type: 'timestamp' })
+  date: Date;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
