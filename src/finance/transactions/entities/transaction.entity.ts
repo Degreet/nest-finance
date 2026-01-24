033a0899dd233761f3bb6fb3d6c68c3e544a70d6
Degreet <degreetpro@gmail.com>
2026-01-24 11:04:31 +0200

    feat(transactions): add TransactionType enum

--- src/finance/transactions/entities/transaction.entity.ts
+++ src/finance/transactions/entities/transaction.entity.ts
@@ -7,14 +7,15 @@ import {
   UpdateDateColumn,
 } from 'typeorm';
 import { Account } from '../../accounts/entities/account.entity';
+import { TransactionType } from '../enums/transaction-type.enum';
 
 @Entity()
 export class Transaction {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
-  type: 'income' | 'expense';
+  @Column({ type: 'enum', enum: TransactionType })
+  type: TransactionType;
 
   @ManyToOne(() => Account, (account) => account.transactions)
   account: Account;
