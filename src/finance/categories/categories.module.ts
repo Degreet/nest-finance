3dd0ebd87847b9c40a5532baa8fbba89df4c3981
Degreet <degreetpro@gmail.com>
2026-02-09 09:13:47 +0200

    feat(categories): seed default categories on user registration

--- src/finance/categories/categories.module.ts
+++ src/finance/categories/categories.module.ts
@@ -3,10 +3,11 @@ import { CategoriesService } from './categories.service';
 import { CategoriesController } from './categories.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Category } from './entities/category.entity';
+import { SeedDefaultCategoriesListener } from './listeners/seed-default-categories.listener';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Category])],
   controllers: [CategoriesController],
-  providers: [CategoriesService],
+  providers: [CategoriesService, SeedDefaultCategoriesListener],
 })
 export class CategoriesModule {}
--- src/finance/categories/categories.service.ts
+++ src/finance/categories/categories.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { Category } from './entities/category.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { PREDEFINED_CATEGORIES } from './categories.constants';
 
 @Injectable()
-export class CategoriesService {}
+export class CategoriesService {
+  constructor(
+    @InjectRepository(Category)
+    private readonly categoryRepository: Repository<Category>,
+  ) {}
+
+  async seedDefaultCategories(userId: number) {
+    const categories = PREDEFINED_CATEGORIES.map(({ name, type }) => {
+      return this.categoryRepository.create({
+        name,
+        type,
+        user: { id: userId },
+      });
+    });
+    await this.categoryRepository.save(categories);
+  }
+}
