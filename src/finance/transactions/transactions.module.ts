47003eea40dcdca00a369fc7b6086a6b4edd0758
Degreet <degreetpro@gmail.com>
2026-02-10 15:11:41 +0200

    refactor(transactions): remove unnecessary Account entity dependency

--- src/finance/transactions/transactions.module.ts
+++ src/finance/transactions/transactions.module.ts
@@ -3,13 +3,12 @@ import { TransactionsService } from './transactions.service';
 import { TransactionsController } from './transactions.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Transaction } from './entities/transaction.entity';
-import { Account } from '../accounts/entities/account.entity';
 import { RecordTransactionHandler } from './commands/record-transaction/record-transaction.handler';
 import { AdjustTransactionHandler } from './commands/adjust-transaction/adjust-transaction.handler';
 import { VoidTransactionHandler } from './commands/void-transaction/void-transaction.handler';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Transaction, Account])],
+  imports: [TypeOrmModule.forFeature([Transaction])],
   controllers: [TransactionsController],
   providers: [
     TransactionsService,
