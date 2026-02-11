3f30b874da12d872be3f2c39623b304ee597ef5b
Degreet <degreetpro@gmail.com>
2026-02-11 15:17:00 +0200

    feat(health): make endpoint public

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
+import { Auth } from './iam/authentication/decorators/auth.decorator';
+import { AuthType } from './iam/authentication/enums/auth-type.enum';
 
+@Auth(AuthType.None)
 @Controller()
 export class AppController {
   @Get('health')
