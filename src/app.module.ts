7f10968edca7170524b6ce1932b5da837451674d
Degreet <degreetpro@gmail.com>
2026-02-11 13:41:04 +0200

    refactor(db): enable synchronize option for development mode only

--- src/app.module.ts
+++ src/app.module.ts
@@ -43,7 +43,7 @@ import { CqrsModule } from '@nestjs/cqrs';
             ? { rejectUnauthorized: true }
             : false,
           autoLoadEntities: true,
-          synchronize: process.env.NODE_ENV !== 'production',
+          synchronize: process.env.NODE_ENV === 'development',
           logging: process.env.NODE_ENV === 'development',
         };
       },
